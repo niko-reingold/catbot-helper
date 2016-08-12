8efefa78922a5f09e51938e4cbe6cbb7720b3e66
Niko Reingold <nreingold@nreingold-air.local>
2016-08-12 09:07:09 -0400

    debug

--- test.js
+++ test.js
@@ -69,6 +69,7 @@ app.get('/call', function (req, res){
 			from: secondNumber,
 			callbackUrl: 'https://' + req.get('host') + '/call',
 			callbackHttpMethod: 'POST'
+		});
 	}, 5000);
 
 	setTimeout(function(){
